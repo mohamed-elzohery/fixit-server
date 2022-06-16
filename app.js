fd88e6cd6839896c47ce848dad4335728ff36c50
mohamed-elzohery <m.zohery1998@gmail.com>
2022-06-16 13:21:57 +0200

    Adding app configuration

---
+++ app.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const morgan = require('morgan');
+const path = require('path');
+const cors = require('cors');
+
+const errorHandler = require('./middleware/errorHandler');
+
+
+const app = express();
+
+app.use(morgan('dev'));
+app.use(express.json());
+app.use(cors());
+
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+
+
+// handle Previous URL Of middleware
+app.all('*', (req, res) => {
+  res.json({
+    status: 'Failure',
+    message: 'wrong url',
+  });
+});
+
+// global error Handler
+app.use(errorHandler);
+
+module.exports = app;
