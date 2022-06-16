8ac22cafd11d0aed373b2025b1eeba3e991d6611
mohamed-elzohery <m.zohery1998@gmail.com>
2022-06-16 13:25:15 +0200

    Adding server configuration

---
+++ server.js
@@ -0,0 +1,14 @@
+const dotenv = require('dotenv');
+const app = require('./app');
+
+const connecteDB = require('./helper/connectDB');
+
+dotenv.config('.env');
+
+const { PORT, DB_URL } = process.env;
+
+connecteDB(DB_URL);
+
+app.listen(PORT, () => {
+  console.log(`server is running on port ${process.env.PORT}`);
+});
