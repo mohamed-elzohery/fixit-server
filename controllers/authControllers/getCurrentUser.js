fac772895acec8d73d9b1534eb9820d1a2869298
mohamed-elzohery <m.zohery1998@gmail.com>
2022-06-16 13:38:58 +0200

    ALogin and get current user

---
+++ controllers/authControllers/getCurrentUser.js
@@ -0,0 +1,23 @@
+const jwt = require('jsonwebtoken');
+const catchAsync = require('../../middlewares/catchAsync');
+const User = require('../../models/User');
+
+const getCurrentUser = catchAsync(async (req, res, next) => {
+    const token = req.cookies['token_uid'];
+    if(!token){
+         return res.send({success: true, data: {currentUser: null}, message: 'token is not available.'});
+    }
+    const decodedToken = jwt.verify(token, process.env.JWT_SECRET);
+    const id = decodedToken.id;
+     if (!id.match(/^[0-9a-fA-F]{24}$/)) {
+        return res.send({success: true, data: {currentUser: null}, message: 'token is not valid.'});
+     }
+     const user = await User.findById(decodedToken.id);
+     if(!user){
+     return res.send({success: true, data: {currentUser: null}, message: 'token is not valid.'});
+     }
+     res.send({success: true, data: {currentUser: user}, message: 'valid token'})
+});
+
+export default getCurrentUser;
+ 
---
+++ controllers/authControllers/login.js
@@ -0,0 +1,24 @@
+const jwt = require('jsonwebtoken');
+const catchAsync = require('../../middlewares/catchAsync');
+const ErrorResponse = require('../../utils/ErrorResponse');
+const User = require('../../models/User');
+
+const login = catchAsync(async (req, res, next) => {
+    const {email, password: enteredPassword} = req.body;
+    if(!(email && enteredPassword)){
+        return next(new ErrorResponse(400, 'Please enter email and password.', 'LoginErr'));
+    }
+    const user = await User.findOne({email});
+    if(!(user !== null && await user.isPasswordsMatched(enteredPassword))){
+        return next(new ErrorResponse(400, 'Invalid email or password.', 'LoginErr'))
+    }
+    const token = user.createToken();
+    res.cookie('token_uid', token, {
+        httpOnly: true,
+        expires: new Date(Date.now() + +process.env.JWT_AGE),
+        path: '/'
+    });
+    res.json({success: true, data: user, token, message: 'User is logged in successfully.'});
+});
+
+export default login;
